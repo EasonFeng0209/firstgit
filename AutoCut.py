f2f950a2fd6f77a64844ef25c5730a1654efd8b4
Eason Feng <2259803759@qq.com>
2020-12-02 15:07:00 +0800

    add s sample file

---
+++ AutoCut.py
@@ -0,0 +1,21 @@
+import cv2
+
+img = cv2.imread(r"D:\pythonExp\opencv\cars.jpg")
+#mw = img.size[0]
+#mh = img.size[1]
+
+a = 100
+b = 60
+for y in range(7): 
+    y0 = y * b
+	
+    for x in range(28):
+        x0 = x * a
+        #cv2.rectangle(img, (x0,y0), (x0+a,y0+b), (0,0,255), 2)
+        cut_img = img[y0:y0+b, x0:x0+a]
+
+        cv2.imwrite("D:\pythonExp\opencv\cuts\cut" + str(y) + str(x) + ".jpg",cut_img)
+        cv2.imshow('image', cut_img)
+        cv2.waitKey(20)
+
+cv2.waitKey(0)
